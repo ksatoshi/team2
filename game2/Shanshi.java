4c9122d8c4bf6ab196b2ce203ddf95898a32e8df
amazing441 <17101@hakodate.kosen-ac.jp>
2021-11-04 11:36:03 +0900

    (藩士を追加)

---
+++ game2/Shanshi.java
@@ -0,0 +1,33 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Shanshi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Shanshi extends Actor
+{
+    /**
+     * Act - do whatever the Shanshi wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    private GreenfootImage img_bkup = null;
+    private int width=50;
+    private int height= 50;
+
+    public Shanshi(){
+        img_bkup = new GreenfootImage( getImage() );
+        getImage().scale( width,height );
+    }
+
+    public void act() 
+    {
+        // Add your action code here.
+
+        GreenfootImage img = new GreenfootImage(img_bkup);
+        img.scale( width,height );
+        setImage(img);
+
+    }      
+}
