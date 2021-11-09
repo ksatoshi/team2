ccd3b784ba83d1a1401df7a51fc492f30176713a
KSatoshi <ksatoshi@mail.ksatoshi.dev>
2021-11-09 11:38:05 +0900

    シャクシャインに当たり判定処理用のテンプレートを追加

--- game2/MyWorld.java
+++ game2/MyWorld.java
@@ -15,19 +15,7 @@ public class MyWorld extends World
      */
     public MyWorld()
     {    
-
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-
         super(1600, 900, 1); 
-        addObject( new Saksaynu(), 100, 396 );
-
-        addObject( new Chanshi(), 1200, 600 );
-        addObject( new Shanshi(), 1350, 200 );
-        addObject( new Shanshi(), 1350, 750 );
-
-
-        
-        
         addObject( new Syakusyain(), 100, 396 );
         addObject( new Chanshi(), 1200, 350 );
         addObject( new Chanshi(), 1200, 650 );
